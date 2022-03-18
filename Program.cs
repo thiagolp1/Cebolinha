35cce8af284e8207b31b9fa5bb6d5b0a3e5f5f9f
thiago <e107ti@cps.sp.gov.br>
2022-03-18 20:24:47 -0300

    terminei a atividade

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿String frase, fraseCebola;
+
+Console.Write("Digite uma flase: ");
+frase = Console.ReadLine()!;
+
+fraseCebola = frase
+.Replace("r", "l")
+.Replace("R", "L");
+Console.WriteLine(fraseCebola);
